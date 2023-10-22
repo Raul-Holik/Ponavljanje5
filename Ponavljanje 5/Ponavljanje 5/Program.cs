4691a0fc44f4bc820e86afff99d2dd9a5a742c0f
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-10-23 00:08:43 +0200

    Add files via upload

---
+++ Ponavljanje 5/Ponavljanje 5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            Console.WriteLine("Upisite URL adresu");
+            str = Console.ReadLine();
+
+            if (str.Substring(0, 4) == "www." && str.Substring(str.Length - 3) == ".hr")
+            {
+                Console.WriteLine("URL je tocan");
+            }
+            else
+            {
+                Console.WriteLine("URL nije tocan");
+            }
+            Console.ReadKey();
+        }
+    }
+}
